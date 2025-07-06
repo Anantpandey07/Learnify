129e27de47514cebc7ca72d2d0e299ee061137ff
Anant <anantpandey175@gmail.com>
2025-07-06 23:33:20 +0530

    HeroSection Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 import { Button } from './components/ui/button'
 import Login from './pages/login'
 import Navbar from './components/Navbar'
+import HeroSection from './pages/student/HeroSection'
 
 function App() {
 
   return (
     <main>
       <Navbar/>
+      <HeroSection/>
       <Login/>
     </main>
   )
